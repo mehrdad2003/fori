0181db3a0b01f91f2f957cadef9215d2034bd1f1
unknown <mehrdadzare2003@gmail.com>
2021-07-04 20:01:33 +0430

    Updates

---
+++ precache-manifest.c21c94c896bc129baee4a33dbf19328d.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc840f9c06be973bf8f839e88c9ecc8e",
+    "url": "/fori/index.html"
+  },
+  {
+    "revision": "a3644096805660cc9fe6",
+    "url": "/fori/static/css/2.586e2826.chunk.css"
+  },
+  {
+    "revision": "6b7b4bb1ffa600bcbc55",
+    "url": "/fori/static/css/main.198ffd79.chunk.css"
+  },
+  {
+    "revision": "a3644096805660cc9fe6",
+    "url": "/fori/static/js/2.2b2e9185.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/fori/static/js/2.2b2e9185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7b4bb1ffa600bcbc55",
+    "url": "/fori/static/js/main.91c69ecd.chunk.js"
+  },
+  {
+    "revision": "f6c7c5e18386ccb83efe",
+    "url": "/fori/static/js/runtime-main.abe631c8.js"
+  },
+  {
+    "revision": "bab5e043e4fc4bcce51699f943003c85",
+    "url": "/fori/static/media/IRANSansWeb.bab5e043.ttf"
+  },
+  {
+    "revision": "0d66bae1fd729304a0d24d61976009b4",
+    "url": "/fori/static/media/cs1.0d66bae1.jpg"
+  },
+  {
+    "revision": "205e0926ddb16facfdee123bbd6a1634",
+    "url": "/fori/static/media/cs3.205e0926.jpg"
+  },
+  {
+    "revision": "91b84d3fb6889206eafa1803a9600643",
+    "url": "/fori/static/media/cs4.91b84d3f.jpg"
+  },
+  {
+    "revision": "18dfd6bb20156ad9ae615bdc549a71d6",
+    "url": "/fori/static/media/cs6.18dfd6bb.jpg"
+  },
+  {
+    "revision": "4908aec5c54a7c926de7643108dec030",
+    "url": "/fori/static/media/f1.4908aec5.jpg"
+  },
+  {
+    "revision": "8633a1d50c9dcb023de176fa5392c3a8",
+    "url": "/fori/static/media/f4.8633a1d5.jpg"
+  },
+  {
+    "revision": "1daad82bfbc0486095e25af803e7648e",
+    "url": "/fori/static/media/found.1daad82b.jpg"
+  },
+  {
+    "revision": "425ed9441fc00c6b15f143a11512c50a",
+    "url": "/fori/static/media/h1.425ed944.jpg"
+  },
+  {
+    "revision": "83a278cf74deeaf72bf7ba9d20a47678",
+    "url": "/fori/static/media/h2.83a278cf.jpg"
+  },
+  {
+    "revision": "ac7050d659f819a46c830295534bd2a5",
+    "url": "/fori/static/media/h3.ac7050d6.jpg"
+  },
+  {
+    "revision": "0d2c19ffbd7772c466e1dc830145617d",
+    "url": "/fori/static/media/h4.0d2c19ff.jpg"
+  },
+  {
+    "revision": "94a3f1a62de49956e796b16127158041",
+    "url": "/fori/static/media/icon0.94a3f1a6.PNG"
+  },
+  {
+    "revision": "867d2705f911f432fdc8374f3c287906",
+    "url": "/fori/static/media/icon1.867d2705.PNG"
+  },
+  {
+    "revision": "66958a5f92014e9770d74dafd1945634",
+    "url": "/fori/static/media/icon2.66958a5f.PNG"
+  },
+  {
+    "revision": "ec886a68f4cba0e4623b6e17f8a4f6f6",
+    "url": "/fori/static/media/icon3.ec886a68.PNG"
+  },
+  {
+    "revision": "795ea6a6c1975fefbbb7787066677d3f",
+    "url": "/fori/static/media/icon4.795ea6a6.PNG"
+  },
+  {
+    "revision": "a098d8faceae1b2f840eec65e9cb6f5e",
+    "url": "/fori/static/media/mag1.a098d8fa.jpg"
+  },
+  {
+    "revision": "dd68bf2e936b80b16fc94bc71ca8053d",
+    "url": "/fori/static/media/mag1.dd68bf2e.PNG"
+  },
+  {
+    "revision": "169602404aa39c68ca891eee47e2b078",
+    "url": "/fori/static/media/mag2.16960240.PNG"
+  },
+  {
+    "revision": "e6220462b88ad63607fae544c372f970",
+    "url": "/fori/static/media/mag2.e6220462.jpg"
+  },
+  {
+    "revision": "591bc174cbe753b8af35ba67b0a4ccde",
+    "url": "/fori/static/media/mag3.591bc174.PNG"
+  },
+  {
+    "revision": "dba172f611540ff6b806f1fb403c806e",
+    "url": "/fori/static/media/mag4.dba172f6.PNG"
+  },
+  {
+    "revision": "9055adc818253518a0a8f2afb5c61e2c",
+    "url": "/fori/static/media/mag5.9055adc8.PNG"
+  },
+  {
+    "revision": "0d82b1e340b1436c391201cb5982f4c8",
+    "url": "/fori/static/media/mag6.0d82b1e3.PNG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/fori/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/fori/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/fori/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/fori/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2300531f380ea934b03f1f3415e3f579",
+    "url": "/fori/static/media/title.2300531f.gif"
+  },
+  {
+    "revision": "f86b2d5100023240b7409aee4bbb8019",
+    "url": "/fori/static/media/v1.f86b2d51.jpg"
+  },
+  {
+    "revision": "4ac6945215719cd933ad66497e9b5405",
+    "url": "/fori/static/media/v10.4ac69452.jpg"
+  },
+  {
+    "revision": "49c9e750718c2b01f8d070f511ac600c",
+    "url": "/fori/static/media/v11.49c9e750.jpg"
+  },
+  {
+    "revision": "de694584c49ca44d238dd8f6793e705c",
+    "url": "/fori/static/media/v12.de694584.jpg"
+  },
+  {
+    "revision": "7aa2ed42692b5de73d332d771394a372",
+    "url": "/fori/static/media/v2.7aa2ed42.jpg"
+  },
+  {
+    "revision": "338c15e7036a26fcccfa71c2219fb724",
+    "url": "/fori/static/media/v4.338c15e7.jpg"
+  },
+  {
+    "revision": "22df2ad2729de5d0aa31fcdefbc5b5a2",
+    "url": "/fori/static/media/v5.22df2ad2.jpg"
+  },
+  {
+    "revision": "a074a791768be53b467a8874ea0a5ac7",
+    "url": "/fori/static/media/v6.a074a791.jpg"
+  },
+  {
+    "revision": "5f69d00b67a402f315e0df81e67aa56c",
+    "url": "/fori/static/media/v8.5f69d00b.jpg"
+  },
+  {
+    "revision": "48fd129a11ad3511430f7a86fe011cc1",
+    "url": "/fori/static/media/v9.48fd129a.jpg"
+  },
+  {
+    "revision": "9cac762cd80deb3ae0eec965b3de4f50",
+    "url": "/fori/static/media/what2.9cac762c.PNG"
+  },
+  {
+    "revision": "0cabf381766fbe8983539bf9bd8a0d01",
+    "url": "/fori/static/media/whatsup.0cabf381.PNG"
+  }
+]);
